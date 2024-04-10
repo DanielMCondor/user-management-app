e6fe11bcb972fec98ac855725493ccc28966f388
DanielMCondor <dmantillacondor@gmail.com>
2024-04-10 16:50:10 -0500

    clear queryClient

--- user-management-fe/src/mocks/render-with-providers.tsx
+++ user-management-fe/src/mocks/render-with-providers.tsx
@@ -2,5 +2,5 @@ import React, { ReactNode } from 'react';
 import { render } from '@testing-library/react';
 import { QueryClient, QueryClientProvider } from 'react-query';
 
-const queryClient = new QueryClient();
+export const queryClient = new QueryClient();
 export const renderWithProviders = (ui: ReactNode) => render(<QueryClientProvider client={queryClient}>{ui}</QueryClientProvider>);
